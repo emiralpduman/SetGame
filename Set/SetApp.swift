0b6b1394e1add634d52f5b9ed4eea3d54ec0fcf3
Emiralp <emiralpduman@gmail.com>
2022-06-18 17:45:10 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Emiralp Duman on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
