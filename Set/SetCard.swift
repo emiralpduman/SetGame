5edf82b2ed5ac5100d7ae00f434076a47a0402af
Emiralp <emiralpduman@gmail.com>
2022-06-24 18:51:10 +0300

    SetGame initializes its deck with 81 cards

---
+++ Set/SetCard.swift
@@ -0,0 +1,34 @@
+//
+//  Card.swift
+//  SetCard
+//
+//  Created by Emiralp Duman on 18.06.2022.
+//
+
+import Foundation
+
+struct SetCard {
+    let numberOfShapes: Number
+    let shape: Shape
+    let shading: Shading
+    let color: Color
+    
+    
+    enum Number: CaseIterable {
+        case one, two, threee
+    }
+
+    enum Shape: CaseIterable {
+        case diamond, squiggle, oval
+    }
+
+    enum Shading: CaseIterable {
+        case solid, striped, open
+    }
+
+    enum Color: CaseIterable {
+        case red, green, purple
+    }    
+}
+
+
--- Set/SetGame.swift
+++ Set/SetGame.swift
@@ -6,10 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct SetGame {
+    private struct Rules {
+        static let numberOfCards: Int = 81
+    }
+    
     //81 piece
-    var cards: [SetCard]
+    var cards = [SetCard]()
+    
+    init() {
+        for _ in 0..<SetGame.Rules.numberOfCards {
+            for numberOfNewCard in SetCard.Number.allCases {
+                for shapeOfNewCard in SetCard.Shape.allCases {
+                    for shadingOfNewCard in SetCard.Shading.allCases {
+                        for colorOfNewCard in SetCard.Color.allCases {
+                            cards.append(SetCard(numberOfShapes: numberOfNewCard, shape: shapeOfNewCard, shading: shadingOfNewCard, color: colorOfNewCard))
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+    }
     
     
     
